b33d07c0ea521476dcb2caca44965656356ed66c
Denys Vitali <denys@denv.it>
2025-03-25 01:02:27 +0100

    fix: edit sysctl.conf directly

--- input/stages/60/08-sysctl.sh
+++ input/stages/60/08-sysctl.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cat << EOF > "${ROOTFS_PATH}"/etc/sysctl.d/90-inotify.conf
+cat << EOF > "${ROOTFS_PATH}"/etc/sysctl.conf
 fs.inotify.max_user_instances = 1024
 fs.inotify.max_user_watches = 524288
 EOF
