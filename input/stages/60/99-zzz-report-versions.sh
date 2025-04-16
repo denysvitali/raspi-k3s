2bc90a6d3d466e250ea11bbc8d6e96f86d7d7bc1
Denys Vitali <denys@denv.it>
2025-04-16 23:26:56 +0200

    build: fix report-versions

--- input/stages/60/99-zzz-report-versions.sh
+++ input/stages/60/99-zzz-report-versions.sh
@@ -4,38 +4,34 @@
 VERSIONS_FILE="${OUTPUT_PATH}/versions.txt"
 
 # Helper function to append content to versions file
-append_to_versions() {
+add_to_v() {
     echo "$1" >> "$VERSIONS_FILE"
 }
 
 # Helper function to append section header to versions file
 append_section() {
-    append_to_versions ""
-    append_to_versions "### $1"
+    add_to_v ""
+    add_to_v "### $1"
 }
 
 
 # System Information section
 append_section "System Information"
-chroot_exec cat /etc/alpine-release | sed 's/^/Alpine Linux: /' >> "$VERSIONS_FILE"
-chroot_exec uname -a >> "$VERSIONS_FILE"
+append_section "$(chroot_exec cat /etc/alpine-release 2>&1)"
+add_to_v "$(uname -a >> "$VERSIONS_FILE" 2>&1)"
 
 # K3s Information section
 append_section "K3s Information"
-chroot_exec k3s --version >> "$VERSIONS_FILE"
+add_to_v "$(chroot_exec k3s --version 2>&1)"
 
 # Package Versions section
 append_section "Package Versions"
-chroot_exec apk info -v k3s tailscale linux-firmware-brcm dropbear | sort >> "$VERSIONS_FILE"
+add_to_v "$(chroot_exec apk list -I | awk '{print $1}')"
 
 # Network Configuration section
 append_section "Network Configuration"
 if [ "$WIFI_ENABLED" = "true" ]; then
-    append_to_versions "WiFi: Enabled (SSID: ${WIFI_SSID})"
+    add_to_v "WiFi: Enabled (SSID: ${WIFI_SSID})"
 else
-    append_to_versions "WiFi: Disabled"
+    add_to_v "WiFi: Disabled"
 fi
-
-# Print message that versions file is ready
-echo "Version information generated at $VERSIONS_FILE"
-cat "$VERSIONS_FILE"
