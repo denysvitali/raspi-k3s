e7b2a69840ad17bd12d0d92159290412e396b00d
Denys Vitali <denys@denv.it>
2025-04-29 23:15:48 +0200

    fix(deps): add tcpdump, use k3s from main repo

--- input/stages/60/03-deps.sh
+++ input/stages/60/03-deps.sh
@@ -1,22 +1,20 @@
 #!/bin/sh
 
-# Add edge repository
-echo "@edge-community http://dl-cdn.alpinelinux.org/alpine/edge/community" >> ${ROOTFS_PATH}/etc/apk/repositories
-
 # install deps and add services to runlevels
 chroot_exec apk add \
   --no-cache \
   dropbear-scp \
-  htop \
-  linux-firmware-brcm \
-  k3s@edge-community \
-  openssh-sftp-server \
-  tailscale \
-  python3 \
-  vim \
-  eudev \
   e2fsprogs \
-  util-linux
+  eudev \
+  htop \
+  k3s \
+  linux-firmware-brcm \
+  openssh-sftp-server \
+  python3 \
+  tailscale \
+  tcpdump \
+  util-linux \
+  vim
 
 # Add services to runlevels
 chroot_exec rc-update add k3s default
